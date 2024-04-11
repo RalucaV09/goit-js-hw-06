e98b5e0dcda7efa15d8dc3fc17616f94f69880cf
RalucaV09 <ralucav09@gmail.com>
2024-04-11 23:25:16 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,21 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const ul = document.getElementById('ingredients');
-
-const fragment = document.createDocumentFragment();
+const ulIngredients = document.getElementById("ingredients");
 
 ingredients.forEach(ingredient => {
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.textContent = ingredient;
-  li.classList.add('item');
-  fragment.appendChild(li);
+  li.classList.add("item");
+  ulIngredients.appendChild(li);
 });
-
-ul.appendChild(fragment);
