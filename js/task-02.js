d2eacc6f5f86ff4ae23ecd4bfb4c1cf8e64ea960
RalucaV09 <ralucav09@gmail.com>
2024-04-11 23:28:08 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,20 @@
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
+document.addEventListener("DOMContentLoaded", function() {
+  const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
 
-const ulIngredients = document.getElementById("ingredients");
+  const ulIngredients = document.getElementById("ingredients");
 
-ingredients.forEach(ingredient => {
-  const li = document.createElement("li");
-  li.textContent = ingredient;
-  li.classList.add("item");
-  ulIngredients.appendChild(li);
+  ingredients.forEach(ingredient => {
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    li.classList.add("item");
+    ulIngredients.appendChild(li);
+  });
 });
+
