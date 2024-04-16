f4fc49b03d4444f43076bcb8093df0ef50df4fd0
RalucaV09 <ralucav09@gmail.com>
2024-04-16 21:53:41 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,8 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 const changeColorButton = document.querySelector('.change-color');
 const colorSpan = document.querySelector('.color');
 
