ebed8a6533f72000f1c6c3f2aa386d8a4005461a
RalucaV09 <ralucav09@gmail.com>
2024-04-16 21:58:04 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', function() {
+  const input = document.querySelector('input[type="number"]');
+  const amount = input.value;
+
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener('click', function() {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
