15e9baa4958cb11da628c6a1b9bf88e6ed752e78
RalucaV09 <ralucav09@gmail.com>
2024-04-16 21:48:17 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,20 +2,20 @@ const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener('submit', function(event) {
   event.preventDefault();
-
+ 
   const formData = new FormData(loginForm);
-
+  
   const formValues = {};
 
   for (let [name, value] of formData) {
-    formValues[name] = value;
+    formValues[name] = value.trim(); 
   }
 
   if (!formValues.email || !formValues.password) {
     alert('Please fill in all fields');
   } else {
     console.log(formValues);
-    
+
     loginForm.reset();
   }
 });
