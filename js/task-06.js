0318cc06dcf6c589f735eacbb5cccce07be36e46
RalucaV09 <ralucav09@gmail.com>
2024-04-16 21:03:16 +0300

    update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,19 @@
 const validationInput = document.getElementById("validation-input");
 
+
 validationInput.addEventListener("blur", function() {
   
   const maxLength = parseInt(validationInput.getAttribute("data-length"), 10);
-  const inputLength = validationInput.value.length;
+  
+  const inputLength = validationInput.value.trim().length;
+  
   
   if (inputLength === maxLength) {
+    
     validationInput.classList.add("valid");
     validationInput.classList.remove("invalid");
   } else {
+    
     validationInput.classList.add("invalid");
     validationInput.classList.remove("valid");
   }
